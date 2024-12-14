ed05b8962ff088bc8732f1960ba14b7685a11f4b
yp-ww <123287726+yp-ww@users.noreply.github.com>
2024-12-14 23:32:04 +0900

    add the initial setting script

---
+++ setting.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd $HOME
+sudo apt update
+
+# Install apt packages
+sudo apt install -y \
+  seclists \
+  enum4linux-ng \
+  ffuf \
+  mlocate \
+  ftp \
+  jd-gui \
+  cadaver \
+  peass \
+  exploitdb
+
+# Create Tools directory
+mkdir -p ~/Tools/Linux
+mkdir -p ~/Tools/Windows
+mkdir -p ~/Tools/Common
+
+# --- Linux --- #
+# Install pspy binaries
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32 -O ~/Tools/Linux/pspy32
+chmod +x ~/Tools/Linux/pspy32
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -O ~/Tools/Linux/pspy64
+
+# Install chisel binaries
+wget https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_linux_amd64.gz -O ~/Tools/Linux/chisel_1.10.1_linux_amd64.gz
+gunzip ~/Tools/Linux/chisel_1.10.1_linux_amd64.gz
+chmod +x ~/Tools/Linux/chisel_1.10.1_linux_amd64
+rm ~/Tools/Linux/chisel_1.10.1_linux_amd64.gz
+
+# --- Windows --- #
+# Windows Exploit Suggester
+git clone https://github.com/Pwnistry/Windows-Exploit-Suggester-python3.git ~/Tools/Windows/Windows-Exploit-Suggester-python3
+
+# PSTools
+wget https://download.sysinternals.com/files/PSTools.zip -O ~/Tools/Windows/PSTools.zip
+unzip ~/Tools/Windows/PSTools.zip -d ~/Tools/Windows/PSTools
+rm ~/Tools/Windows/PSTools.zip
+
+# pypykatz
+python3 -m venv myenv
+source ~/myenv/bin/activate
+pip3 install pypykatz
+deactivate
+
+
+
+
+
