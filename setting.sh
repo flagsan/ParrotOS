87b74bccf999d20e9dd3577f9e4d311ce97b3bf8
yp-ww <123287726+yp-ww@users.noreply.github.com>
2024-12-15 18:34:35 +0900

    update python venv to Poetry

--- setting.sh
+++ setting.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+# Prerequires: git clone https://github.com/yp-ww/ParrotOS.git
 
 cd $HOME
 sudo apt update
+
+# Create Tools directory
+mkdir -p ~/Tools/Linux
+mkdir -p ~/Tools/Windows
+mkdir -p ~/Tools/Common
+
+# Install Poetry dependencies
+cp ./poetry.lock ~/Tools
+cp ./pyproject.toml ~/Tools
+poetry --directory ~/Tools install
 
 # Install apt packages
 sudo apt install -y \
@@ -15,17 +26,13 @@ sudo apt install -y \
   peass \
   exploitdb
 
-# Create Tools directory
-mkdir -p ~/Tools/Linux
-mkdir -p ~/Tools/Windows
-mkdir -p ~/Tools/Common
-
 # --- Linux --- #
-# Pspy
+# Install pspy binaries
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32 -O ~/Tools/Linux/pspy32
+chmod +x ~/Tools/Linux/pspy32
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -O ~/Tools/Linux/pspy64
 
-# Chisel
+# Install chisel binaries
 wget https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_linux_amd64.gz -O ~/Tools/Linux/chisel_1.10.1_linux_amd64.gz
 gunzip ~/Tools/Linux/chisel_1.10.1_linux_amd64.gz
 chmod +x ~/Tools/Linux/chisel_1.10.1_linux_amd64
@@ -39,14 +46,3 @@ git clone https://github.com/Pwnistry/Windows-Exploit-Suggester-python3.git ~/To
 wget https://download.sysinternals.com/files/PSTools.zip -O ~/Tools/Windows/PSTools.zip
 unzip ~/Tools/Windows/PSTools.zip -d ~/Tools/Windows/PSTools
 rm ~/Tools/Windows/PSTools.zip
-
-# Pypykatz
-python3 -m venv myenv
-source ~/myenv/bin/activate
-pip3 install pypykatz
-deactivate
-
-
-
-
-
